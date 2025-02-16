624c8cfd23a234fea8b1ba5a7ae1b91bf932d041
kdrm <kdonram@gmail.com>
2025-02-16 15:45:28 +0530

    Create a .env file and add the url to index.js file

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import mongoose from 'mongoose';
 import { productRouter } from './routes/productRouter.js'; 
 import { userRouter } from './routes/userRouter.js';   
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-const mongoUrl = "mongodb+srv://admin:123456%40Tom@cbc-db.42xmr.mongodb.net/?retryWrites=true&w=majority&appName=cbc-db";
+const mongoUrl = process.env.MONGO_DB_URL;
 
 mongoose.connect(mongoUrl,{});
 const connection = mongoose.connection;
