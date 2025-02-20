a9f25c0d160aaf621d4e87c4fc7af900c54ee2ea
kdrm <kdonram@gmail.com>
2025-02-20 22:44:35 +0530

    Create Order Schema

---
+++ Models/order.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+
+const Order = mongoose.Schema({
+    orderId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    orderedItem: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            price: {
+                type: String,
+                required: true
+            },
+            quantity: {
+                type: String,
+                required: true
+            },
+            image: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    paymentId: {
+        type: String
+    },
+    notes: {
+        type: String,
+    },
+    status: {
+        type: String,
+        default: "Preparing"
+    },
+    name: {
+        type : String,
+        required: true
+    }
+})
