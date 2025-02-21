12a0ae4084f02bb32a30ac7ccc7b1ecee7a68f5d
kdrm <kdonram@gmail.com>
2025-02-21 20:50:56 +0530

    create a function to get the orders from the database.

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import { createOrder } from '../controllers/orderController.js';
+import { createOrder,getOrders } from '../controllers/orderController.js';
 
 export const orderRouter = express.Router();
 
 orderRouter.post('/',createOrder);
+
+orderRouter.get('/',getOrders);
